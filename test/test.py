0ecb9cfe0b619ebe44fab5c0c86ecd3485ef61d9
ephrenm <ephrenmanning@gmail.com>
2024-09-03 17:54:08 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,6 +4,7 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge
 
 # uio_out[0] is ADC CS
 # uio_out[3] is ADC CLK
@@ -19,7 +20,6 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 125, units="ns")
     cocotb.start_soon(clock.start())
 
@@ -34,22 +34,7 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    for i in range(255):
-        dut.ui_in.value = i
-        await ClockCycles(dut.clk, 1)
+    dut.ui_in.value = 100
+    await ClockCycles(dut.clk, 10)
 
     assert dut.uo_out.value == 0
-
-    # # Set the input values you want to test
-    # dut.ui_in.value = 20
-    # dut.uio_in.value = 30
-
-    # # Wait for one clock cycle to see the output values
-    # await ClockCycles(dut.clk, 100000)
-
-    # # The following assersion is just an example of how to check the output values.
-    # # Change it to match the actual expected output of your module:
-    # assert dut.uo_out.value == 0
-
-    # # Keep testing the module by changing the input values, waiting for
-    # # one or more clock cycles, and asserting the expected output values.
